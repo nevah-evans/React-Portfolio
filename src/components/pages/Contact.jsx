356798394a6dc8e49817a397f4f4bec7a20d38d9
Nevah <165732847+nevah-evans@users.noreply.github.com>
2024-09-13 20:15:55 -0500

    adding Contact form and styling

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,12 +1,37 @@
 import Header from "../Header";
 
-export default function Contact () {
-    return (
-        <div id="contact-page">
-            <Header />
-            <p>
-                This is the Contact page of the portfolio.
-            </p>
+export default function Contact() {
+  return (
+    <div id="contact-page">
+      <Header />
+
+      <form id="form">
+        <div class="row input-group">
+          <label class="col-name-label">Name</label>
+          <div class="col-sm-3">
+          <input type="text" aria-label="name" class="form-control"></input>
+          </div>
         </div>
-    );
+
+        <div class="row mb-6">
+          <label for="email" class="col-form-label">Email Address</label>
+          <div class="col-sm-3">
+            <input type="email" class="form-control" id="email"></input>
+          </div>
+        </div>
+
+        <div class="row input-group">
+          <label class="col-message-label">Message</label>
+          <div class="col-sm-3">
+          <textarea class="form-control" aria-label="Message"></textarea>
+        </div>
+        </div>
+
+        <div class="col-sm-3">
+        <button type="submit" id="submit">Submit</button>
+        </div>
+
+      </form>
+    </div>
+  );
 }
